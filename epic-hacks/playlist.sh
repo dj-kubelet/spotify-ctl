fdc36b0ba54aadbaa57b62c8375f2a71ed8dd927
Mattias Appelgren <mattias@ppelgren.se>
2019-06-10 22:29:41 +0200

    Epic Playlist Hack :shipit:

---
+++ epic-hacks/playlist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+ACCESS_TOKEN=$(kubectl get secrets spotify-oauth -ojsonpath='{.data.accesstoken}' | base64 -d)
+
+playlist_id=$1
+
+curl -s -H "Authorization: Bearer $ACCESS_TOKEN" \
+    "https://api.spotify.com/v1/playlists/$playlist_id/tracks" |
+    jq -r '.items[].track.uri' |
+    while read -r uri; do
+        ./queue.sh "$uri"
+    done
