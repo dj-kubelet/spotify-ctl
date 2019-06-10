34cc3c37104efb277a07858a037044d845f9de02
Gustav Westling <hello@gustav.tv>
2019-06-10 15:21:38 +0200

    Use a part of the queue input as the job prefix
    Example:
    
    $ ./queue.sh "Satan i gatan"
    job.batch/track-satan-i-gatan-gfbfv created

--- epic-hacks/queue.sh
+++ epic-hacks/queue.sh
@@ -3,4 +3,13 @@ set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-kubectl create -f <(TRACK="$*" envsubst < "$DIR/job.tmpl.yaml")
+TRACK="$*"
+
+# Create a Kubernetes friendly name of the track search input
+JOB_NAME=${TRACK// /-} # remove spaces from $TRACK
+JOB_NAME=${JOB_NAME//:/-} # replace : with -
+JOB_NAME=$(echo "$JOB_NAME" | tr [:upper:] [:lower:]) # lowercase
+JOB_NAME=$(echo "$JOB_NAME" | LANG=c tr -cd '[:print:]') # remove non ascii characters
+JOB_NAME=$(echo "$JOB_NAME" | cut -c-253) # trim to max 253 characters
+
+kubectl create -f <(TRACK="$TRACK" JOB_NAME="$JOB_NAME" envsubst < "$DIR/job.tmpl.yaml")
