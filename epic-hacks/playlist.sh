aa9b5c6ed6fd662e4918d5204b8cddb95dacc803
Gustav Westling <hello@gustav.tv>
2019-06-11 09:30:33 +0200

    Execute playlist.sh from anywhere, and cleanup input data :tada:

--- epic-hacks/playlist.sh
+++ epic-hacks/playlist.sh
@@ -3,11 +3,14 @@ set -euo pipefail
 
 ACCESS_TOKEN=$(kubectl get secrets spotify-oauth -ojsonpath='{.data.accesstoken}' | base64 --decode)
 
-playlist_id=$1
+# Remove "spotify:playlist:" from the input if present
+playlist_id=${1/spotify:playlist:/}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 curl -s -H "Authorization: Bearer $ACCESS_TOKEN" \
     "https://api.spotify.com/v1/playlists/$playlist_id/tracks" |
     jq -r '.items[].track.uri' |
     while read -r uri; do
-        ./queue.sh "$uri"
+        "$DIR/queue.sh" "$uri"
     done
