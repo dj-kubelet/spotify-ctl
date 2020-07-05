01c0306f24bdeba180c87744b1b56f99633d0409
Mattias Appelgren <mattias@ppelgren.se>
2020-07-05 17:17:57 +0200

    Use snake_case in oauth-secret

--- epic-hacks/playlist.sh
+++ epic-hacks/playlist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-ACCESS_TOKEN=$(kubectl get secrets spotify-oauth -ojsonpath='{.data.accesstoken}' | base64 --decode)
+ACCESS_TOKEN=$(kubectl get secrets spotify-oauth -ojsonpath='{.data.access_token}' | base64 --decode)
 
 # Remove "spotify:playlist:" from the input if present
 playlist_id=${1/spotify:playlist:/}
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 curl -s -H "Authorization: Bearer $ACCESS_TOKEN" \
     "https://api.spotify.com/v1/playlists/$playlist_id/tracks" |
